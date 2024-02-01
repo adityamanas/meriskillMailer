dfcbb4cfb6f55886267c426dfbb386413814fe98
adityamanas <aditya.manas01@gmail.com>
2024-02-01 18:29:23 +0530

    Ui fix

---
+++ sendEmails.js
@@ -0,0 +1,101 @@
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const fs = require("fs/promises");
+const path = require("path");
+const { promisify } = require("util");
+const puppeteer = require("puppeteer");
+
+async function generatePDF(htmlContent) {
+  try {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    await page.setContent(htmlContent);
+
+    const pdfBuffer = await page.pdf({ format: "A4" });
+
+    await browser.close();
+
+    return pdfBuffer;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function sendEmailWithAttachment(
+  to,
+  subject,
+  emailTemplate,
+  pdfTemplate,
+  data
+) {
+  try {
+    const emailTemplatePath = path.join(
+      __dirname,
+      "templates",
+      `${emailTemplate}.ejs`
+    );
+    const emailTemplateContent = await fs.readFile(emailTemplatePath, "utf-8");
+    const compiledEmailTemplate = ejs.compile(emailTemplateContent)(data);
+
+    const pdfTemplatePath = path.join(
+      __dirname,
+      "templates",
+      `${pdfTemplate}.ejs`
+    );
+    const pdfTemplateContent = await fs.readFile(pdfTemplatePath, "utf-8");
+    const compiledPdfTemplate = ejs.compile(pdfTemplateContent)(data);
+
+    const pdfBuffer = await generatePDF(compiledPdfTemplate);
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "hrtechcertifysolution@gmail.com",
+        pass: "jerhrfxedbegowav",
+      },
+    });
+
+    const pdfFileName = `${data.name}_offerLetter.pdf`;
+
+    const mailOptions = {
+      from: "hrtechcertifysolution@gmail.com",
+      to,
+      subject,
+      html: compiledEmailTemplate,
+      attachments: [
+        {
+          filename: pdfFileName,
+          content: pdfBuffer,
+          encoding: "base64",
+        },
+      ],
+    };
+
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent successfully to ${to}`);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+}
+
+const recipients = [
+  { email: "aditya.manas01@gmail.com", name: "aditya" },
+  { email: "manas.23aug@gmail.com", name: "pandey" },
+];
+
+Promise.all(
+  recipients.map((recipient) =>
+    sendEmailWithAttachment(
+      recipient.email,
+      "Job Offer",
+      "offerLetter",
+      // "frontend",
+      // "pdfTemplate",
+      "frontendcopy",
+      { name: recipient.name }
+    )
+  )
+)
+  .then(() => console.log("All emails with PDF sent successfully"))
+  .catch((error) => console.error("Error sending emails:", error));
